aedb0e8f20d4f48270672b8240dc4e482d8ef8a7
Dean Cooper <dean_cooper@hotmail.com>
2016-09-13 14:28:41 +1000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+$( document ).ready(function() {
+
+  $('.cb-slideshow').hide()
+
+  $("#typedLine").typed({
+    strings: ["Welcome to...^500", "Vision Software^500"],
+    typeSpeed: 100,
+    callback: function(){
+      console.log('typed js callback.');
+      $('.cb-slideshow').show();
+      $('.typed-cursor').hide();
+      $("#typedLine").hide();
+    }
+  });
+
+});
+
+
+/*
+$("#instructionLabel").typed({
+     strings: ["Welcome. Before you can make a booking",
+     "You need to pass a simple coding test."],
+     typeSpeed: 0,
+     callback: function() {
+         $('.typed-cursor').show();
+           $('#submitQuizButton').show();
+           console.log('running quiz from typed callback');
+       runQuiz();
+     }
+    }
+   );
+
+   */
